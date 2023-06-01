b6824b36d4c1fe22864cb5a55dec169e3b831400
wuyue <wuyue92tree@163.com>
2023-06-01 16:53:36 +0800

    fix ADMINLTE_CONFIG_CLASS

--- adminlteui/templatetags/adminlte_core.py
+++ adminlteui/templatetags/adminlte_core.py
@@ -10,7 +10,13 @@ register = template.Library()
 @register.simple_tag
 def get_adminlte_config_class():
     if hasattr(settings, 'ADMINLTE_CONFIG_CLASS'):
-        return importlib.import_module(settings.ADMINLTE_SETTINGS)
+        m = '.'.join(settings.ADMINLTE_CONFIG_CLASS.split('.')[:-1])
+        c = settings.ADMINLTE_CONFIG_CLASS.split('.')[-1]
+        module = importlib.import_module(m)
+        class_ = getattr(module, c)
+        if not isinstance(class_(), AdminLteConfig):
+            raise Exception(f'invalid AdminLteConfig class: {c}.')
+        return class_()
     else:
         return AdminLteConfig()
 
